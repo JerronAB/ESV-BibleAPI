bf2eaf7cc2ff91786d65e6562e9b60b7ad200114
JerronAB <92005206+JerronAB@users.noreply.github.com>
2025-01-17 10:00:02 -0600

    Update curl_test.ps1

--- curl_test.ps1
+++ curl_test.ps1
@@ -1,29 +1,5 @@
 # List of verses
 $verses = @(
-    "1John 4:7-12"
-    "Lamentations 3:22-23"
-    "2Corinthians 4:17"
-    "Matthew 6:27"
-    "Romans 12:9-14"
-    "Romans 12:15-19"
-    "Romans 8:38-39"
-    "Psalm 46:5"
-    "John 16:33"
-    "Matthew 6:34"
-    "Romans 12:21"
-    "Isaiah 30:18"
-    "1John 4:18-19"
-    "John 8:11"
-    "Titus 3:4-5"
-    "Romans 12:2"
-    "Matthew 17:20"
-    "1Peter 4:8"
-    "Isaiah 25:8"
-    "Romans 8:2"
-    "Romans 8:3-5"
-    "Romans 8:6-11"
-    "John 1:7-19"
-    "Matthew 11:28-30"
     "1Peter 1:6"
     "1John 4:13-17"
     "1Corinthians 16:13"
@@ -34,13 +10,21 @@ $verses = @(
     "Midian"
     "beloved's"
     "Judges 1:1"
+    "love"
+    "hate"
+    "and"
+    "there"
 )
 
-foreach ($verse in $verses) {
-    # Replace spaces with + for URL encoding
-    $encoded_verse = $verse -replace ' ', '+'
-    $url = "http://localhost/api/$encoded_verse"
-    Write-Output "Fetching data for verse: $encoded_verse"
-    Invoke-RestMethod -Method Get -Uri $url
-    Write-Output "`n"
+while ($true) {
+    $verses | ForEach-Object -ThrottleLimit 10 -Parallel {
+        $verse = $_
+        # Replace spaces with + for URL encoding
+        $encoded_verse = $verse -replace ' ', '+'
+        #$url = "http://localhost/api/$encoded_verse"
+        $url = "https://esv.obky-gas.com/api/$encoded_verse"
+        Write-Output "Fetching data for verse: $encoded_verse"
+        Invoke-RestMethod -Method Get -Uri $url
+        Write-Output "`n"
+    }
 }
